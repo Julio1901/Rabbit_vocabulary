dffadad70665b497bcc6be308a9e33a2790388ef
Julio1901 <js.cesar42@gmail.com>
2022-01-28 15:41:57 -0300

    Word translate game screen started

---
+++ app/src/main/java/com/julio/rabbitvocabulary/fragments/TranslateTheWordGameFragment.kt
@@ -0,0 +1,20 @@
+package com.julio.rabbitvocabulary.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.julio.rabbitvocabulary.R
+
+class TranslateTheWordGameFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_translate_the_word_game, container, false)
+    }
+
+}
